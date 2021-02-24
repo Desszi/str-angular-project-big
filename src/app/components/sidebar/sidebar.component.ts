e664e41bb3ba77c0287844a0dd13b7c4376847e0
ormiati <attila.ormos@gmail.com>
2021-02-24 21:37:20 +0100

    icon jav

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,8 +10,8 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/table-list', title: 'Termékek',  icon:'content_paste', class: '' },
-    { path: '/address-list', title: 'Csak a címek',  icon:'ibrary_books', class: '' },
-    { path: '/customer-list', title: 'Vásárlók',  icon:'ibrary_books', class: '' },
+    { path: '/address-list', title: 'Csak a címek',  icon:'library_books', class: '' },
+    { path: '/customer-list', title: 'Vásárlók',  icon:'library_books', class: '' },
 
     { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
     { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
