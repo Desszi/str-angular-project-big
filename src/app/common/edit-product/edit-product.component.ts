4ef598888a8c1b4f4621194281dde5ad7b9e6d89
Margit Robert <marrob50hz@gmail.com>
2021-02-25 11:10:14 +0100

    toaster added to edit-porduct comp

--- src/app/common/edit-product/edit-product.component.ts
+++ src/app/common/edit-product/edit-product.component.ts
@@ -3,8 +3,8 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'app/model/product';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs-compat/operator/switchMap';
-import { ProductsService} from '../../service/products.service';
+import { ProductsService } from '../../service/products.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-edit-product',
@@ -13,37 +13,44 @@ import { ProductsService} from '../../service/products.service';
 })
 export class EditProductComponent implements OnInit {
 
-  product:Product = new Product();
+  product: Product = new Product();
 
   constructor(
-    private productsService:ProductsService, 
-    private activatedRoute:ActivatedRoute,
+    private productsService: ProductsService,
+    private activatedRoute: ActivatedRoute,
     private router: Router,
+    private toastr: ToastrService,
 
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(
-      params=> 
-          this.productsService.getById(params.id).subscribe(
-          item =>{
+      params =>
+        this.productsService.getById(params.id).subscribe(
+          item => {
             console.log(item);
             this.product = item || new Product();
           }
         )
-     )
-    }
+    )
+  }
 
+  onUpdate(form: NgForm, item: Product): void {
 
-    onUpdate(form: NgForm, item: Product): void {
-      if(item.id == 0){
+    try {
+      if (item.id == 0) {
         this.productsService.update(item);
+        this.toastr.success('Sikeresn hozzáadásra került');
         this.router.navigate(['/table-list']);
       }
-      else{
-        this.productsService.update(item)
-        this.router.navigate(['/table-list']);    
+      else {
+        this.productsService.update(item);
+        this.toastr.success('Sikeres módosítás :)');
+        this.router.navigate(['/table-list']);
       }
-    }
+    } catch (error) {
 
+      this.toastr.success('Probléma történt:' + error);
+    }
+  }
 }
