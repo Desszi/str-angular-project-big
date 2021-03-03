db748330fbc96388b5d0badcb1c0821e6883eb1d
Leo <takib77@gmail.com>
2021-03-03 10:19:40 +0100

    angular cards

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutCard } from 'app/model/about-card';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+
+  cards: AboutCard[] = [
+    { id: 1, fullName: 'KIRÁLY ÁRPÁD', image: '', html: 85, css_scss: 75, javascript: 65, angular: 100, git: '' },
+    { id: 2, fullName: 'MARGIT RÓBERT', image: '', html: 80, css_scss: 75, javascript: 65, angular: 50, git: '' },
+    { id: 3, fullName: 'ORMOS ATTILA', image: '', html: 85, css_scss: 43, javascript: 65, angular: 50, git: '' },
+    { id: 4, fullName: 'PORKOLÁB MERCÉDESZ', image: '', html: 85, css_scss: 81, javascript: 65, angular: 50, git: '' },
+    { id: 5, fullName: 'SZŰCS TAMÁS', image: '../assets/img/faces/SzT.jpg', html: 85, css_scss: 75, javascript: 65, angular: 50, git: 'https://github.com/Oborsil/' },
+    { id: 6, fullName: 'TAKÁCS BÁLINT', image: '', html: 66, css_scss: 75, javascript: 65, angular: 50, git: '' },
+    { id: 7, fullName: 'VILLÁNYI ÁGNES', image: '', html: 85, css_scss: 75, javascript: 99, angular: 50, git: '' },
+    { id: 8, fullName: 'GONDA VALÉR', image: '', html: 97, css_scss: 75, javascript: 25, angular: 50, git: '' }
+  ]
 
   constructor() { }
 
