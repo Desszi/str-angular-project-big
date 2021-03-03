dc791dbda66cf693bc2ab3d727be07336c816937
ormiati <attila.ormos@gmail.com>
2021-03-03 19:36:13 +0100

    about me

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -11,7 +11,7 @@ export class AboutComponent implements OnInit {
   cards: AboutCard[] = [
     { id: 1, fullName: 'KIRÁLY ÁRPÁD', image: '', html: 85, css_scss: 75, javascript: 65, angular: 100, git: '' },
     { id: 2, fullName: 'MARGIT RÓBERT', image: '../assets/img/faces/marrob.jpg', html: 80, css_scss: 75, javascript: 65, angular: 50, git: 'https://github.com/marrob/' },
-    { id: 3, fullName: 'ORMOS ATTILA', image: '', html: 85, css_scss: 43, javascript: 65, angular: 50, git: '' },
+    { id: 3, fullName: 'ORMOS ATTILA', image: '../assets/img/faces/Attila.jpg', html: 85, css_scss: 65, javascript: 45, angular: 50, git: 'https://github.com/ormiati/' },
     { id: 4, fullName: 'PORKOLÁB MERCÉDESZ', image: '', html: 85, css_scss: 81, javascript: 65, angular: 50, git: '' },
     { id: 5, fullName: 'SZŰCS TAMÁS', image: '../assets/img/faces/SzT.jpg', html: 85, css_scss: 75, javascript: 65, angular: 50, git: 'https://github.com/Oborsil/' },
     { id: 6, fullName: 'TAKÁCS BÁLINT', image: '', html: 87, css_scss: 67, javascript: 57, angular: 47, git: 'https://github.com/takib77/' },
