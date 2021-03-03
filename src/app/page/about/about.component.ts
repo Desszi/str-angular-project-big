2a68d0ade048de2107a1e0f66779899b8001b016
Arpad <solar@hu.inter.net>
2021-03-03 21:45:53 +0100

    kiraly

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -9,7 +9,7 @@ import { AboutCard } from 'app/model/about-card';
 export class AboutComponent implements OnInit {
 
   cards: AboutCard[] = [
-    { id: 1, fullName: 'KIRÁLY ÁRPÁD', image: '', html: 85, css_scss: 75, javascript: 65, angular: 100, git: '' },
+    { id: 1, fullName: 'KIRÁLY ÁRPÁD', image: '../assets/img/faces/ka.jpg', html: 70, css_scss: 55, javascript: 52, angular: 52, git: 'https://github.com/arpad58/' },
     { id: 2, fullName: 'MARGIT RÓBERT', image: '../assets/img/faces/marrob.jpg', html: 80, css_scss: 75, javascript: 65, angular: 50, git: 'https://github.com/marrob/' },
     { id: 3, fullName: 'ORMOS ATTILA', image: '../assets/img/faces/Attila.jpg', html: 85, css_scss: 65, javascript: 45, angular: 50, git: 'https://github.com/ormiati/' },
     { id: 4, fullName: 'PORKOLÁB MERCÉDESZ', image: '', html: 85, css_scss: 81, javascript: 65, angular: 50, git: '' },
