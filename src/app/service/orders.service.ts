3385f74e275268479393a1bdf04438d1f4d2a0e7
Leo <takib77@gmail.com>
2021-03-03 11:27:21 +0100

    Common style in table-lists vol2

--- src/app/service/orders.service.ts
+++ src/app/service/orders.service.ts
@@ -10,15 +10,15 @@ import { Column } from 'app/model/column';
   providedIn: 'root'
 })
 export class OrdersService extends BaseService<Order> {
-  constructor(httpClient: HttpClient, config:ConfigService) {
+  constructor(httpClient: HttpClient, config: ConfigService) {
     super(config, httpClient, "orders");
   }
 
-  columns:Column[] = [
-    {index:0, name:'id', title:'#', type:'text', sortDir:''},
-    {index:1, name:'customerID', title:'VÁSÁRLÓ az.', type:'text', sortDir:''},
-    {index:2, name:'productID', title:'TERMÉK az.', type:'text', sortDir:''},
-    {index:3, name:'amount', title:'ÖSSZEG', type:'text', sortDir:''},
-    {index:4, name:'status', title:'ÁLLAPOT', type:'text', sortDir:''},
+  columns: Column[] = [
+    { index: 0, name: 'id', title: '#', type: 'text', sortDir: '' },
+    { index: 1, name: 'customerID', title: 'VÁSÁRLÓ SZÁMA', type: 'text', sortDir: '' },
+    { index: 2, name: 'productID', title: 'TERMÉK SZÁMA', type: 'text', sortDir: '' },
+    { index: 3, name: 'amount', title: 'ÖSSZEG', type: 'text', sortDir: '' },
+    { index: 4, name: 'status', title: 'ÁLLAPOT', type: 'text', sortDir: '' },
   ]
 }
