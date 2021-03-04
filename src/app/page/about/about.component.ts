3693406420612197a5a52d9cdb5414bee31844a8
Leo <takib77@gmail.com>
2021-03-04 20:09:33 +0100

    about desszi

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -12,10 +12,10 @@ export class AboutComponent implements OnInit {
     { id: 1, fullName: 'KIRÁLY ÁRPÁD', image: '../assets/img/faces/ka.jpg', html: 70, css_scss: 55, javascript: 52, angular: 52, git: 'https://github.com/arpad58/' },
     { id: 2, fullName: 'MARGIT RÓBERT', image: '../assets/img/faces/marrob.jpg', html: 80, css_scss: 75, javascript: 65, angular: 50, git: 'https://github.com/marrob/' },
     { id: 3, fullName: 'ORMOS ATTILA', image: '../assets/img/faces/Attila.jpg', html: 85, css_scss: 65, javascript: 45, angular: 50, git: 'https://github.com/ormiati/' },
-    { id: 4, fullName: 'PORKOLÁB MERCÉDESZ', image: '', html: 85, css_scss: 81, javascript: 65, angular: 50, git: '' },
+    { id: 4, fullName: 'PORKOLÁB MERCÉDESZ', image: '../assets/img/faces/desszi.jpg', html: 95, css_scss: 88, javascript: 93, angular: 67, git: 'https://github.com/Desszi/' },
     { id: 5, fullName: 'SZŰCS TAMÁS', image: '../assets/img/faces/SzT.jpg', html: 85, css_scss: 75, javascript: 65, angular: 50, git: 'https://github.com/Oborsil/' },
     { id: 6, fullName: 'TAKÁCS BÁLINT', image: '../assets/img/faces/balint.jpg', html: 87, css_scss: 77, javascript: 67, angular: 57, git: 'https://github.com/takib77/' },
-    { id: 7, fullName: 'VILLÁNYI ÁGNES', image: '', html: 85, css_scss: 75, javascript: 99, angular: 50, git: '' },
+    { id: 7, fullName: 'VILLÁNYI ÁGNES', image: '', html: 85, css_scss: 75, javascript: 99, angular: 50, git: 'https://github.com/Afonya74' },
     { id: 8, fullName: 'GONDA VALÉR', image: '', html: 97, css_scss: 75, javascript: 25, angular: 50, git: '' }
   ]
 
